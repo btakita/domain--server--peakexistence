ce49135e1ff064288dc741d3998215b607724245
Brian Takita <brian.takita@gmail.com>
2024-03-28 02:35:58 -0400

    ./feature: + psychedelic_therapy

---
+++ feature/psychedelic_therapy.ts
@@ -0,0 +1,19 @@
+import { type feature_T } from './feature.js'
+export const psychedelic_therapy = <feature_T>{
+	title: `Psychedelic Therapy`,
+	slug: 'psychedelic-therapy',
+	description: `Dr. Emenike offers a revolutionary approach to holistic healing. Combining:
+
+- his expertise in internal medicine
+- cutting-edge research on psychedelic-assisted therapies
+- the proven benefits of exercise
+`.trim(),
+	tag_a1: [
+		'psychedelic-therapy',
+		'anxiety',
+		'ketamine',
+		'well-being',
+	],
+	// hero_image: exercise_with_oxygen_therapy_webp,
+	hero_image: 'https://placehold.co/1020x520.webp',
+}
