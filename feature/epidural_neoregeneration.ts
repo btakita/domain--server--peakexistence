2b11a9838bc137ca0f3178ec879b2e438b0513e0
Brian Takita <brian.takita@gmail.com>
2024-04-02 17:37:05 -0400

    ./feature: + epidural_neoregeneration

---
+++ feature/epidural_neoregeneration.ts
@@ -0,0 +1,15 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const epidural_neoregeneration = <feature_T>{
+	title: 'Epidural Neoregeneration',
+	slug: 'epidural-neoregeneration',
+	description: `Epidural Neuroregeneration is a frontier in pain management and spinal health. It's about healing nerve damage. This method employs the epidural space near the spine. The goal? To foster nerve repair and reduce pain.`,
+	tag_a1: [
+		'epidural-neoregeneration',
+		'epidural',
+		'neoregeneration',
+		'spine-health',
+		'nerve-damage',
+		'anti-aging',
+	],
+	hero_image: placeholder_hero_image,
+}
