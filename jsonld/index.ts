8566abffa93fdedd491a973f952b89a81c126a76
Brian Takita <brian.takita@gmail.com>
2024-04-03 00:06:38 -0400

    + ./json:
    	+ dr_manny_emenike_Person_id_ref_
    	+ dr_manny_emenike_Person_id_
    	+ brian_takita_Person_id_ref_
    	+ brian_takita_Person_id_

---
+++ jsonld/index.ts
@@ -0,0 +1,42 @@
+import { ns_id_be_id_ref_id_jsonld_pair_ } from '@rappstack/domain--server/jsonld'
+import { site__author_a1_ } from '@rappstack/domain--server/site'
+import { import_meta_env_ } from 'ctx-core/env'
+import { nullish__none_, tup } from 'ctx-core/rmemo'
+import { type request_ctx_T } from 'relysjs/server'
+const ns = import_meta_env_().NODE_ENV === 'production' ? 'app' : ''
+export const [
+	[dr_manny_emenike_Person_id_ref_,],
+	dr_manny_emenike_Person_id_
+] = [
+	ns_id_be_id_ref_id_jsonld_pair_(ns, 'Person', ctx=>{
+		return nullish__none_(
+			tup(site__author_a1_(ctx)), (
+				site__author_a1
+			)=>{
+				const dr_manny_emenike = site__author_a1[0]
+				if (dr_manny_emenike.name !== 'Dr. Manny Emenike') {
+					throw Error('author.name !== \'Dr. Manny Emenike\'')
+				}
+				return dr_manny_emenike
+			})
+	}),
+	(ctx:request_ctx_T)=>dr_manny_emenike_Person_id_ref_(ctx)!['@id']
+]
+export const [
+	[brian_takita_Person_id_ref_,],
+	brian_takita_Person_id_
+] = [
+	ns_id_be_id_ref_id_jsonld_pair_(ns, 'Person', ctx=>{
+		return nullish__none_(
+			tup(site__author_a1_(ctx)), (
+				site__author_a1
+			)=>{
+				const brian_takita = site__author_a1[1]
+				if (brian_takita.name !== 'Brian Takita') {
+					throw Error('author.name !== \'Brian Takita\'')
+				}
+				return brian_takita
+			})
+	}),
+	(ctx:request_ctx_T)=>brian_takita_Person_id_ref_(ctx)!['@id']
+]
