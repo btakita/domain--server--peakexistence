97ab521927747f55671f5283a42aab14391b90a7
Brian Takita <brian.takita@gmail.com>
2024-04-05 01:06:19 -0400

    ./feature: + genetic_testing

---
+++ feature/genetic_testing.ts
@@ -0,0 +1,18 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const genetic_testing = <feature_T>{
+	title: 'Genetic Testing',
+	description: `Genetic testing is a transformative tool, offering insights deep within our biological blueprints. This approach not paves the way for personalized medicine. And empowers individuals with knowledge about their genetic predispositions.`,
+	slug: 'genetic-testing',
+	tag_a1: [
+		'genetic-testing',
+		'genetic',
+		'health-check',
+		'health-test',
+		'screening',
+		'health-screening',
+		'testing',
+		'body-scan',
+		'non-surgical',
+	],
+	hero_image: placeholder_hero_image,
+}
