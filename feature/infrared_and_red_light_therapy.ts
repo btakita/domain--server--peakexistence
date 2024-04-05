0b0173f62c62ec985b50b349e076d25fb5bfbed7
Brian Takita <brian.takita@gmail.com>
2024-04-05 18:10:01 -0400

    ./feature: red_light_therapy→infrared_and_red_light_therapy

--- feature/infrared_and_red_light_therapy.ts
+++ feature/infrared_and_red_light_therapy.ts
@@ -1,9 +1,12 @@
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
-export const red_light_therapy = <feature_T>{
-	title: 'Red Light Therapy',
+export const infrared_and_red_light_therapy = <feature_T>{
+	title: 'Infrared & Red Light Therapy',
 	description: `The sun's warmth on your skin is a comforting sensation. Red light therapy treatments harness specific wavelengths of light. To penetrate the skin, aiming to rejuvenate, heal, and energize at a cellular level.`,
-	slug: 'red-light-therapy',
+	slug: 'infrared-and-red-light-therapy',
 	tag_a1: [
+		'infrared-and-red-light-therapy',
+		'infrared-light',
+		'infrared-light-therapy',
 		'red-light-therapy',
 		'red-light',
 		'light-therapy',
