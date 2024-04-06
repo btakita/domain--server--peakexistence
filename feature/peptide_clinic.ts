1a18906423bbfa1e9731fe2a1d65684bb96a9d0a
Brian Takita <brian.takita@gmail.com>
2024-04-05 20:21:50 -0400

    ./feature: + peptide_clinic

---
+++ feature/peptide_clinic.ts
@@ -0,0 +1,19 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const peptide_clinic = <feature_T>{
+	title: 'Peptide Clinic',
+	description: `Peptides are short chains of amino acids. Peptides are the building blocks of proteins in the body. They play a pivotal role in numerous biological processes. Peptide clinics specialize in the therapeutic use of peptides. To treat a wide array of conditions and improve health outcomes.`,
+	slug: 'peptide-clinic',
+	tag_a1: [
+		'myers-cocktail-with-nad-and-glutathione',
+		'myers-cocktail',
+		'nad',
+		'glutathione',
+		'vitamin',
+		'vitamin-infusion',
+		'healthy-living',
+		'energy',
+		'cellular-energy',
+		'non-surgical',
+	],
+	hero_image: placeholder_hero_image,
+}
