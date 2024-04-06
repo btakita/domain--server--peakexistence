60d7000f57f54b8e4a80b89decb1ade645e27b29
Brian Takita <brian.takita@gmail.com>
2024-04-06 00:49:46 -0400

    ./feature: + supervised_weight_loss_and_performance

---
+++ feature/supervised_weight_loss_and_performance.ts
@@ -0,0 +1,18 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const supervised_weight_loss_and_performance = <feature_T>{
+	title: 'Supervised Weight Loss and Performance',
+	slug: 'supervised-weight-loss-and-performance',
+	// language=md
+	description: `Supervised weight loss programs include personal guidance and professional oversight. For a structured pathway to reach these weight loss goals. This approach prioritizes safety and effectiveness. And emphasizes performance enhancement. Catering to individuals seeking to improve their physical capabilities alongside weight management.`,
+	tag_a1: [
+		'supervised-weight-loss-and-performance',
+		'supervised-weight-loss',
+		'supervised-performance',
+		'medical-supervision',
+		'weight-training',
+		'cardio',
+		'weight-loss',
+		'non-surgical',
+	],
+	hero_image: placeholder_hero_image,
+}
