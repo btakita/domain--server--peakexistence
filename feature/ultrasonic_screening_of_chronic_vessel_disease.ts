c68911b8d47cefcca46dbe73fdebaaa9269dc68a
Brian Takita <brian.takita@gmail.com>
2024-04-06 03:35:07 -0400

    ./feature: ultrasonic_screening_of_chronic_vessels→ultrasonic_screening_of_chronic_vessel_disease:
    	description: expand copy
    	tag_a1:
    		+ cerebral-atrophy-with-chronic-ischemic-changes
    		+ chronic-small-vessel-disease
    		+ chronic-small-vessel-ischemic-disease
    		+ chronic-ischemic-small-vessel-disease
    		+ chronic-ischemic-microangiopathy
    		+ chronic-microangiopathic-ischemic-disease
    		+ chronic-microvascular-disease
    		+ chronic-microvascular-ischemic-disease
    		+ cerebral-microvascular-ischemic-disease
    		+ chronic-vessel-enlargement
    		+ health-conditions
    		+ heart-health
    		+ peripheral-artery-disease
    		+ vessel-disease
    		+ vessel

---
+++ feature/ultrasonic_screening_of_chronic_vessel_disease.ts
@@ -0,0 +1,33 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const ultrasonic_screening_of_chronic_vessel_disease = <feature_T>{
+	title: 'Ultrasonic Screening of Chronic Vessels',
+	slug: 'ultrasonic-screening-of-chronic-vessel-disease',
+	// language=md
+	description: `Ultrasonic Screening uses high-frequency sound waves. To create detailed images of the body's internal structures & blood vessels. Early detection and intervention are crucial in preventing serious complications from vascular diseases. Chronic vascular diseases are leading causes of morbidity and mortality worldwide. Including heart attacks, strokes, and amputations.`,
+	tag_a1: [
+		'ultrasonic-screening-of-chronic-vessel-disease',
+		'ultrasonic-screening',
+		'chronic-vessel-disease',
+		'cerebral-atrophy-with-chronic-ischemic-changes',
+		'chronic-vessels',
+		'chronic-small-vessel-disease',
+		'chronic-small-vessel-ischemic-disease',
+		'chronic-ischemic-small-vessel-disease',
+		'chronic-ischemic-microangiopathy',
+		'chronic-microangiopathic-ischemic-disease',
+		'chronic-microvascular-disease',
+		'chronic-microvascular-ischemic-disease',
+		'cerebral-microvascular-ischemic-disease',
+		'chronic-vessel-enlargement',
+		'screening',
+		'diagnostics',
+		'health-check',
+		'health-conditions',
+		'heart-health',
+		'peripheral-artery-disease',
+		'vessel-disease',
+		'vessel',
+		'non-surgical',
+	],
+	hero_image: placeholder_hero_image,
+}
