b5d194a60601f37d34028bb2ef6aea956e451d8b
Brian Takita <brian.takita@gmail.com>
2024-04-10 15:58:50 -0400

    ./feature: + cardiovascular_health

---
+++ feature/cardiovascular_health.ts
@@ -0,0 +1,20 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const cardiovascular_health = <feature_T>{
+	title: 'Cardiovascular Health',
+	slug: 'cardiovascular-health',
+	// language=md
+	description: `In the intricate dance of life, the heart plays the lead, setting the rhythm for our body's vital functions. Cardiovascular health is not just about preventing diseases. It's about nurturing the core of our existence. Let's look into the latest advances for a blueprint to longevity and well-being.`,
+	tag_a1: [
+		'atherosclerosis',
+		'cardiovascular',
+		'cardiovascular-health',
+		'cholesterol',
+		'diet',
+		'exercise',
+		'meditation',
+		'mindfulness',
+		'stress-management',
+		'yoga',
+	],
+	hero_image: placeholder_hero_image,
+}
