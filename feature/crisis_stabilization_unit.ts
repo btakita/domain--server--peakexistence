21399063bcb0d961aab3200d842727fe5c2d5c12
Brian Takita <brian.takita@gmail.com>
2024-04-10 22:26:37 -0400

    ./feature: + crisis_stabilization_unit

---
+++ feature/crisis_stabilization_unit.ts
@@ -0,0 +1,14 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const crisis_stabilization_unit = <feature_T>{
+	title: 'Crisis Stabilization Unit',
+	slug: 'crisis-stabilization-unit',
+	// language=md
+	description: `Crisis Stabilization Units are facilities designed to provide rapid assessment. And treatment for individuals experiencing acute mental health crises. Unlike traditional hospital settings, CSUs offer a more targeted approach. Focusing on stabilizing patients over a short period, typically 24 to 72 hours. The goal is to avert the need for hospitalization by providing immediate  care. Crafting a follow-up treatment plan that addresses the individual's long-term needs.`,
+	tag_a1: [
+		'affordable-health-care',
+		'crisis-stabilization-unit',
+		'mental-health',
+		'mental-health-care',
+	],
+	hero_image: placeholder_hero_image,
+}
