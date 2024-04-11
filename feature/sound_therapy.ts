1666ca17de7b21be70191e6e0dc55d8c08be814d
Brian Takita <brian.takita@gmail.com>
2024-04-11 02:47:23 -0400

    ./feature: + sound_therapy

---
+++ feature/sound_therapy.ts
@@ -0,0 +1,18 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const sound_therapy = <feature_T>{
+	title: `Sound Therapy`,
+	description: `Cultures around the word use sound as therapy, for centuries. Modern sound therapy harnesses the power of specific frequencies, vibrations, and music. To promote relaxation, healing, and personal growth.`,
+	slug: 'sound-therapy',
+	tag_a1: [
+		'anxiety',
+		'depression',
+		'meditation',
+		'meditation-and-frequency-based-therapy',
+		'mental-health',
+		'non-surgical',
+		'sound-therapy',
+		'therapy',
+		'well-being',
+	],
+	hero_image: placeholder_hero_image,
+}
