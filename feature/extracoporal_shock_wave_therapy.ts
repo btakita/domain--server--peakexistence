b8e113c886b3d4a2f9115babc0cf5af146fe714c
Brian Takita <brian.takita@gmail.com>
2024-04-11 17:50:08 -0400

    ./feature: + extracoporal_shock_wave_therapy

---
+++ feature/extracoporal_shock_wave_therapy.ts
@@ -0,0 +1,11 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const extracoporal_shock_wave_therapy = <feature_T>{
+	title: `Extracoporal Shock Wave Therapy`,
+	description: ``,
+	slug: 'extracoporal-shock-wave-therapy',
+	tag_a1: [
+		'extracoporal-shock-wave-therapy',
+		'non-surgical',
+	],
+	hero_image: placeholder_hero_image,
+}
