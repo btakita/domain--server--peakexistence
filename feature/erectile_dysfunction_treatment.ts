0fcea456dc9862b75a6955c413402cb98b1eba44
Brian Takita <brian.takita@gmail.com>
2024-04-11 01:21:05 -0400

    ./feature: + erectile_dysfunction_treatment

---
+++ feature/erectile_dysfunction_treatment.ts
@@ -0,0 +1,13 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const erectile_dysfunction_treatment = <feature_T>{
+	title: 'Erectile Dysfunction Treatment',
+	slug: 'erectile-dysfunction-treatment',
+	// language=md
+	description: `Erectile dysfunction (ED) is often viewed through the narrow lens of sexual health. Yet its implications extend far into the broader spectrum of overall well-being. Particularly cardiovascular health. Treating ED offers a unique opportunity not only to improve sexual health. But to address & manage the underlying cardiovascular conditions. `,
+	tag_a1: [
+		'erectile-dysfunction',
+		'erectile-dysfunction-treatment',
+		'preventative-care',
+	],
+	hero_image: placeholder_hero_image,
+}
