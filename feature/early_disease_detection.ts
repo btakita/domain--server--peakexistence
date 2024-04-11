7e61487db15832fa9a9d319461b86b698618176f
Brian Takita <brian.takita@gmail.com>
2024-04-10 23:52:26 -0400

    ./feature: + early_disease_detection

---
+++ feature/early_disease_detection.ts
@@ -0,0 +1,13 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const early_disease_detection = <feature_T>{
+	title: 'Early Disease Detection',
+	slug: 'early-disease-detection',
+	// language=md
+	description: `Diseases often develop gradually, hiding in our bodies before causing noticeable symptoms. Early detection is a superpower in the fight against illness. Offering the best chance for successful treatment, improved outcomes, and saving lives.`,
+	tag_a1: [
+		'disease-detection',
+		'early-disease-detection',
+		'preventative-care',
+	],
+	hero_image: placeholder_hero_image,
+}
