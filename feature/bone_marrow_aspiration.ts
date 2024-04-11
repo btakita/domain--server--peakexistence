29d5649d433af812d64ab02a6586e51ce6b48192
Brian Takita <brian.takita@gmail.com>
2024-04-11 03:24:10 -0400

    ./feature: + bone_marrow_aspiration

---
+++ feature/bone_marrow_aspiration.ts
@@ -0,0 +1,12 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const bone_marrow_aspiration = <feature_T>{
+	title: `Bone Marrow Aspiration`,
+	description: `A bone marrow aspiration is a procedure to obtain a sample of liquid bone marrow for analysis. Bone marrow is the soft, spongy tissue at the center of certain bones. Bone marrow produce blood cells. This test offers valuable insights into blood disorders and certain types of cancer.`,
+	slug: 'bone-marrow-aspiration',
+	tag_a1: [
+		'blood-diagnostics',
+		'bone-marrow-aspiration',
+		'diagnostics'
+	],
+	hero_image: placeholder_hero_image,
+}
