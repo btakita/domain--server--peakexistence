9184c8811a51fcbe51f642863523f71dfd46e71f
Brian Takita <brian.takita@gmail.com>
2024-04-12 00:07:20 -0400

    ./feature: + robotic_hair_and_beard_transplant

---
+++ feature/robotic_hair_and_beard_transplant.ts
@@ -0,0 +1,13 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const robotic_hair_and_beard_transplant = <feature_T>{
+	title: `Robotic Hair and Beard Transplant`,
+	description: `Hair loss, affecting both men and women, can be a source of significant emotional distress. Robotic technology is making its way into the field. Offering a more precise and efficient solution than traditional hair transplantation.`,
+	slug: 'robotic-hair-and-beard-transplant',
+	tag_a1: [
+		'beard-transplant',
+		'hair-transplant',
+		'non-surgical',
+		'robotic-hair-and-beard-transplant',
+	],
+	hero_image: placeholder_hero_image,
+}
