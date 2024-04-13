a826f5bfc53eb30eeb404f22cf2b147e10b61469
Brian Takita <brian.takita@gmail.com>
2024-04-12 22:48:41 -0400

    ./feature: + kyphoplasty_spinal_fracture_treatment

---
+++ feature/kyphoplasty_spinal_fracture_treatment.ts
@@ -0,0 +1,13 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const kyphoplasty_spinal_fracture_treatment = <feature_T>{
+	title: `Kyphoplasty Spinal Fracture Treatment`,
+	description: `Kyphoplasty is a minimally invasive surgical procedure. Designed to treat vertebral compression fractures. Which are often caused by osteoporosis, cancer, or trauma. This procedure alleviates pain & also to stabilize the fracture. Restoring vertebral height. Correcting spinal deformities such as kyphosis.`,
+	slug: 'kyphoplasty-spinal-fracture-treatment',
+	tag_a1: [
+		'kyphoplasty',
+		'kyphoplasty-spinal-fracture-treatment',
+		'osteoporosis',
+		'osteoporosis-fracture-treatment',
+	],
+	hero_image: placeholder_hero_image,
+}
