d89f8f3cfc8faec67d077bbe54d84fca3ecfd952
Brian Takita <brian.takita@gmail.com>
2024-04-12 20:09:22 -0400

    ./feature: + discseel_procedure

---
+++ feature/discseel_procedure.ts
@@ -0,0 +1,11 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const discseel_procedure = <feature_T>{
+	title: `Discseel® Procedure`,
+	description: `Chronic back pain from damaged spinal discs can significantly impact quality of life. The Discseel® Procedure offers a minimally invasive treatment option. Designed to seal tears within spinal discs. Relieving pain and restoring disc function.`,
+	slug: 'discseel-procedure',
+	tag_a1: [
+		'discseel',
+		'discseel-procedure',
+	],
+	hero_image: placeholder_hero_image,
+}
