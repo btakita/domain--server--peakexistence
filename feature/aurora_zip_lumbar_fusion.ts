866638cb8ca58565c791aecc27d41798d0f0100e
Brian Takita <brian.takita@gmail.com>
2024-04-14 23:44:57 -0400

    ./feature: aurora_zip_lumbar_fusion: Aurora ZIP™

--- feature/aurora_zip_lumbar_fusion.ts
+++ feature/aurora_zip_lumbar_fusion.ts
@@ -1,7 +1,7 @@
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
 export const aurora_zip_lumbar_fusion = <feature_T>{
 	title: `Aurora ZIP™ Lumbar Fusion`,
-	description: `Aurora ZIP™ Lumbar Fusion is a newer type of spinal fusion surgery. Designed to promote stability for conditions like degenerative disc disease or spondylolisthesis. Of the lower spine (lumbar region). The Aurora ZIP system utilizes a patented approach that minimizes surgical disruption.`,
+	description: `Aurora ZIP™ Lumbar Fusion is a newer type of spinal fusion surgery. Designed to promote stability for conditions like degenerative disc disease or spondylolisthesis. Of the lower spine (lumbar region). The Aurora ZIP™ system utilizes a patented approach that minimizes surgical disruption.`,
 	slug: 'aurora-zip-lumbar-fusion',
 	tag_a1: [
 		'aurora-zip-lumbar-fusion',
