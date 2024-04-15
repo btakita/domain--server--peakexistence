0472cdc138d2a68ce69601c9fa749a0fc78bac98
Brian Takita <brian.takita@gmail.com>
2024-04-14 23:30:12 -0400

    ./feature: + minimally_invasive_spinal_surgery

---
+++ feature/minimally_invasive_spinal_surgery.ts
@@ -0,0 +1,18 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const minimally_invasive_spinal_surgery = <feature_T>{
+	title: 'Minimally Invasive Spinal Surgery',
+	slug: 'minimally-invasive-spinal-surgery',
+	// language=md
+	description: `Minimally invasive spinal surgery (MISS) offers patients a variety of options. For relief from back pain and other spinal conditions. MISS techniques involve smaller incisions than traditional open surgery. Which has less tissue damage & quicker recovery periods.`,
+	tag_a1: [
+		'aurora-zip-procedure',
+		'back-pain',
+		'discseel-procedure',
+		'kyphoplasty',
+		'minimally-invasive-spinal-surgery',
+		'percutaneous-spine-descompression',
+		'spinal-cord-stimulation',
+		'spine-health',
+	],
+	hero_image: placeholder_hero_image,
+}
