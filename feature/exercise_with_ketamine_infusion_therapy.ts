477fce4fcbc0cea1c241a075f3427fa1337fd505
Brian Takita <brian.takita@gmail.com>
2024-05-03 01:06:57 -0400

    ./feature: exercise_with_ketamine_infusion_therapy: description: more direct description

--- feature/exercise_with_ketamine_infusion_therapy.ts
+++ feature/exercise_with_ketamine_infusion_therapy.ts
@@ -1,7 +1,7 @@
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
 export const exercise_with_ketamine_infusion_therapy = <feature_T>{
 	title: `Exercise with Ketamine Infusion Therapy`,
-	description: `Ketamine is a general anesthetic widely used in operating rooms since the 1970s. Ketamine has shown rapid and profound antidepressant effects, administered in intravenous low doses. Over 20 years of extensive research demonstrates the effectiveness of ketamine infusion therapy. In providing relief for depression and other mental health conditions.`,
+	description: `Integrating exercise with ketamine therapy is an effective treatment of mental health disorders. Particularly depression & anxiety. Ketamine has gained recognition for its rapid-acting antidepressant effects. While exercise is well-documented for its mental health benefits.`,
 	slug: 'exercise-with-ketamine-infusion-therapy',
 	tag_a1: [
 		'exercise',
