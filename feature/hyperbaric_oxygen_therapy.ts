fd64ddbb8394479a5b4234fd920a8f586305d962
Brian Takita <brian.takita@gmail.com>
2024-05-03 15:15:34 -0400

    ./feature: hyperbaric_oxygen_therapy: description,description_md: + long COVID

--- feature/hyperbaric_oxygen_therapy.ts
+++ feature/hyperbaric_oxygen_therapy.ts
@@ -1,16 +1,16 @@
+import { nl } from '@rappstack/ui--any/string'
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
 export const hyperbaric_oxygen_therapy = <feature_T>{
 	title: 'Hyperbaric Oxygen Therapy',
 	slug: 'hyperbaric-oxygen-therapy',
-	description: `Hyperbaric Oxygen Therapy (HBOT) involves administering 100% pure oxygen in a pressurized chamber. Allowing your body to absorb higher levels of oxygen than normal breathing. This increase in oxygen concentration: Promotes healing. Accelerates tissue repair. Enhances the body’s natural ability to fight infections.`,
+	description: `Hyperbaric Oxygen Therapy (HBOT) involves administering 100% pure oxygen in a pressurized chamber. Allowing your body to absorb higher levels of oxygen than normal breathing. This increase in oxygen concentration: Promotes healing. Accelerates tissue repair. Enhances the body’s natural ability to fight infections, including long COVID.`,
 	// language=md
-	description_md: `
-Hyperbaric Oxygen Therapy (HBOT) involves administering 100% pure oxygen in a pressurized chamber. Allowing your body to absorb higher levels of oxygen than normal breathing. This increase in oxygen concentration:
-
-- promotes healing
-- accelerates tissue repair
-- enhances the body’s natural ability to fight infections
-		`.trim(),
+	description_md: [
+		`Hyperbaric Oxygen Therapy (HBOT) involves administering 100% pure oxygen in a pressurized chamber. Allowing your body to absorb higher levels of oxygen than normal breathing. This increase in oxygen concentration:`,
+		``,
+		`- promotes healing`,
+		`- accelerates tissue repair`,
+		`- enhances the body’s natural ability to fight infections, including long COVID`].join(nl),
 	tag_a1: [
 		'hyperbaric',
 		'oxygen',
