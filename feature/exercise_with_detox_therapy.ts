46cef2935ec820d31ae5c743167e679fbb50a2fc
Brian Takita <brian.takita@gmail.com>
2024-05-03 00:14:10 -0400

    ./feature: exercise_with_detox_therapy: and→&

--- feature/exercise_with_detox_therapy.ts
+++ feature/exercise_with_detox_therapy.ts
@@ -1,7 +1,7 @@
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
 export const exercise_with_detox_therapy = <feature_T>{
 	title: `Exercise with Detox Therapy`,
-	description: `This approach offers a comprehensive pathway to enhanced wellness. Blending the dynamic world of exercise with the rejuvenating effects of detox therapy. By harmonizing the body's natural purification processes with targeted physical activity. Individuals can unlock a new level of vitality, clarity, and balance. Exercise with Detox Therapy not only amplifies the detoxifying capabilities. But also sets the stage for a healthier lifestyle. Promising a holistic transformation that resonates with both the body and mind.`,
+	description: `This approach offers a comprehensive pathway to enhanced wellness. Blending the dynamic world of exercise with the rejuvenating effects of detox therapy. By harmonizing the body's natural purification processes with targeted physical activity. Individuals can unlock a new level of vitality, clarity, & balance. Exercise with Detox Therapy not only amplifies the detoxifying capabilities. But also sets the stage for a healthier lifestyle. Promising a holistic transformation that resonates with both the body & mind.`,
 	slug: 'exercise-with-detox-therapy',
 	tag_a1: [
 		'addiction-rehab',
