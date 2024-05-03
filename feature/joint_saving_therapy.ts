1415c3406ec0405fc8fe5f0b7feb04f2c7a24216
Brian Takita <brian.takita@gmail.com>
2024-05-03 18:34:36 -0400

    ./feature: joint_saving_therapy: description: and→&

--- feature/joint_saving_therapy.ts
+++ feature/joint_saving_therapy.ts
@@ -3,7 +3,7 @@ export const joint_saving_therapy = <feature_T>{
 	title: 'Joint Saving Therapy',
 	subtitle: 'Mobility Without Surgery',
 	slug: 'joint-saving-therapy',
-	description: `Joint Saving Therapy (JST) aims to preserve natural joints. It's an alternative to joint replacement surgery. The focus is on reducing pain, improving function, and delaying or avoiding surgery.`,
+	description: `Joint Saving Therapy (JST) aims to preserve natural joints. It's an alternative to joint replacement surgery. The focus is on reducing pain, improving function, & delaying or avoiding surgery.`,
 	tag_a1: [
 		'joint-saving-therapy',
 		'non-surgical',
