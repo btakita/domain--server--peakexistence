5f1bfeb31541b960fa14cfd32e35e9d61a5e5677
Brian Takita <brian.takita@gmail.com>
2024-05-04 01:32:22 -0400

    ./feature: myers_cocktail_with_nad_and_glutathione: description: format

--- feature/myers_cocktail_with_nad_and_glutathione.ts
+++ feature/myers_cocktail_with_nad_and_glutathione.ts
@@ -1,7 +1,7 @@
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
 export const myers_cocktail_with_nad_and_glutathione = <feature_T>{
 	title: 'Myer\'s Cocktail with NAD & Glutathione',
-	description: `The Myer's vitamin Cocktail with NAD (Nicotinamide Adenine Dinucleotide) & Glutathione. This potent infusion offers a powerhouse of nutrients. Designed to rejuvenate the body at a cellular level. `,
+	description: `The Myer's vitamin Cocktail with NAD (Nicotinamide Adenine Dinucleotide) & Glutathione. This potent infusion offers a powerhouse of nutrients. Designed to rejuvenate the body at a cellular level.`,
 	slug: 'myers-cocktail-with-nad-and-glutathione',
 	tag_a1: [
 		'myers-cocktail-with-nad-and-glutathione',
