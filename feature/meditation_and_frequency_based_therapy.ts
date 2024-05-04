6cb9af857eb84b3e294978e347ffea2371a37582
Brian Takita <brian.takita@gmail.com>
2024-05-03 20:10:39 -0400

    ./feature: meditation_and_frequency_based_therapy: description: and→&

--- feature/meditation_and_frequency_based_therapy.ts
+++ feature/meditation_and_frequency_based_therapy.ts
@@ -1,7 +1,7 @@
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
 export const meditation_and_frequency_based_therapy = <feature_T>{
 	title: `Meditation & Frequency Based Therapy`,
-	description: `Meditation & frequency-based therapy stand out as complementary approaches. That harmonize the mind and body, addressing ailments at their core. Working through subtle, yet powerful influences on our minds and bodies.`,
+	description: `Meditation & frequency-based therapy stand out as complementary approaches. That harmonize the mind & body, addressing ailments at their core. Working through subtle, yet powerful influences on our minds & bodies.`,
 	slug: 'meditation-and-frequency-based-therapy',
 	tag_a1: [
 		'anxiety',
