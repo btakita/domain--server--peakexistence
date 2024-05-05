6c495b9554d53319ad5a43ddd4c44f53e70843a4
Brian Takita <brian.takita@gmail.com>
2024-05-05 01:29:52 -0400

    ./feature: performance_improvement: description,description_md: content

--- feature/performance_improvement.ts
+++ feature/performance_improvement.ts
@@ -1,8 +1,19 @@
+import { nl } from '@rappstack/ui--any/string'
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
 export const performance_improvement = <feature_T>{
 	title: `Performance Improvement`,
 	slug: 'performance-improvement',
-	description: `Dr. Emenike is a renowned expert in the field of sports injuries and performance. With a holistic and comprehensive approach that sets him apart. Dr. Emenike works with athletes’ hormones, digestive health, and immune systems. To prevent burnout, injuries, and frequent illnesses. He emphasizes optimizing the body for peak athletic performance and injury prevention.`,
+	// description: `Dr. Emenike is a renowned expert in the field of sports injuries & performance. With a holistic & comprehensive approach that sets him apart. Dr. Emenike works with athletes’ hormones, digestive health, & immune systems. To prevent burnout, injuries, & frequent illnesses. He emphasizes optimizing the body for peak athletic performance & injury prevention.`,
+	description: `In the competitive world of sports, athletes continuously seek methods to enhance their performance, accelerate recovery, & extend their careers. Modern sports science integrates advanced therapies with traditional training. To provide athletes with the edge they need.`,
+	description_md: [
+		`In the competitive world of sports, athletes continuously seek methods to:`,
+		``,
+		`- enhance their performance`,
+		`- accelerate recovery`,
+		`- extend their careers`,
+		``,
+		`Modern sports science integrates advanced therapies with traditional training. To provide athletes with the edge they need.`
+	].join(nl),
 	tag_a1: [
 		'performance-improvement',
 		'hyperbaric-oxygen-therapy',
