99803ab09299e7f7f73c942289ce589ed0507bc1
Brian Takita <brian.takita@gmail.com>
2024-05-27 19:29:35 -0400

    ./feature:
    	+ intractable_headaches_migraines
    	traumatic_brain_injury_concussion_diagnostics_therapy: tag_a1: + tags

---
+++ feature/intractable_headaches_migraines.ts
@@ -0,0 +1,19 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const intractable_headaches_migraines = <feature_T>{
+	title: `Intractable Headaches/Migraines: Diagnostics, Monitoring, & Treatment`,
+	description: `Intractable headaches & migraines are also known as refractory or medication-resistant headaches. The headaches are persistent & debilitating conditions that do not respond to standard treatments. These headaches can impact the quality of life, making effective management crucial.`,
+	slug: 'intractable-headaches-migraines',
+	tag_a1: [
+		'chronic-migraine-treatment',
+		'headache-diagnostic',
+		'headache-monitoring',
+		'headache-treatment',
+		'intractable-headaches',
+		'intractable-migraines',
+		'migraines',
+		'non-pharmacological-migraines-treatments',
+		'non-surgical',
+		'refractory-headache-management',
+	],
+	hero_image: placeholder_hero_image,
+}
