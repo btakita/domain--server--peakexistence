2b18a6e7dfef773298073236e001d8eb064e4e1d
Brian Takita <brian.takita@gmail.com>
2024-05-27 13:51:09 -0400

    ./feature: + ketamine_infusion_therapy

--- feature/exercise_with_ketamine_infusion_therapy.ts
+++ feature/exercise_with_ketamine_infusion_therapy.ts
@@ -1,4 +1,7 @@
 import { type feature_T, placeholder_hero_image } from './_subtle.js'
+/**
+ * Only kept for historical reasons. Do not include in site.
+ */
 export const exercise_with_ketamine_infusion_therapy = <feature_T>{
 	title: `Exercise with Ketamine Infusion Therapy`,
 	description: `Integrating exercise with ketamine therapy is an effective treatment of mental health disorders. Particularly depression & anxiety. Ketamine has gained recognition for its rapid-acting antidepressant effects. While exercise is well-documented for its mental health benefits.`,
---
+++ feature/ketamine_infusion_therapy.ts
@@ -0,0 +1,20 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const ketamine_infusion_therapy = <feature_T>{
+	title: `Ketamine Infusion Therapy`,
+	description: `Ketamine is a promising treatment for major depressive disorder (MDD) & treatment-resistant depression (TRD). This innovative therapy offers rapid relief from depressive symptoms. Particularly for individuals who have not responded to conventional treatments.`,
+	slug: 'ketamine-infusion-therapy',
+	tag_a1: [
+		'exercise',
+		'ketamine',
+		'therapy',
+		'ketamine-therapy',
+		'inflammation-reduction',
+		'enhanced-physical-performance',
+		'well-being',
+		'anxiety',
+		'depression',
+		'mental-health',
+		'non-surgical',
+	],
+	hero_image: placeholder_hero_image,
+}
