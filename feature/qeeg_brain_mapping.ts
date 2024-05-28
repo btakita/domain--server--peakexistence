13c46177ca69e38de1fa3576e801eeb6f7fb6ad3
Brian Takita <brian.takita@gmail.com>
2024-05-27 22:04:07 -0400

    ./feature: + qeeg_brain_mapping

---
+++ feature/qeeg_brain_mapping.ts
@@ -0,0 +1,18 @@
+import { type feature_T, placeholder_hero_image } from './_subtle.js'
+export const qeeg_brain_mapping = <feature_T>{
+	title: `QEEG & Brain Mapping Technology`,
+	description: `Quantitative Electroencephalography (QEEG) is commonly known as brain mapping. QEEG measures the brain's electrical activity to provide detailed insights into its functioning. QEEG is non-invasive. A valuable tool in diagnosing & treating various neurological & psychiatric conditions.`,
+	slug: 'qeeg-brain-mapping',
+	tag_a1: [
+		'brain-mapping-technology',
+		'mental-health',
+		'neurofeedback',
+		'non-surgical',
+		'qeeg-anxiety-therapy',
+		'qeeg-brain-mapping',
+		'qeeg-brain-wave-analysis',
+		'qeeg-diagnostics',
+		'quantitative-electroencephalography',
+	],
+	hero_image: placeholder_hero_image,
+}
