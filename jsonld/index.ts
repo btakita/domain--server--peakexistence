5adc336c97adb7074690640e6cf171ed9fc51349
Brian Takita <brian.takita@gmail.com>
2024-05-31 12:05:51 -0400

    ./jsonld: + site__author_id_ref_a1_

--- jsonld/index.ts
+++ jsonld/index.ts
@@ -1,7 +1,7 @@
 import { ns_id_be_id_ref_id_jsonld_pair_ } from '@rappstack/domain--server/jsonld'
 import { site__author_a1_ } from '@rappstack/domain--server/site'
 import { import_meta_env_ } from 'ctx-core/env'
-import { nullish__none_, tup } from 'ctx-core/rmemo'
+import { ns_id_be_memo_pair_, nullish__none_, tup } from 'ctx-core/rmemo'
 import { type request_ctx_T } from 'relysjs/server'
 const ns = import_meta_env_().NODE_ENV === 'production' ? 'app' : ''
 export const [
@@ -40,3 +40,15 @@ export const [
 	}),
 	(ctx:request_ctx_T)=>brian_takita_Person_id_ref_(ctx)!['@id']
 ]
+export const [
+	,
+	site__author_id_ref_a1_
+] = ns_id_be_memo_pair_(
+	ns,
+	'site__author_id_ref_a1',
+	ctx=>
+		nullish__none_(tup(
+			dr_manny_emenike_Person_id_ref_(ctx),
+			brian_takita_Person_id_ref_(ctx)
+		), (dr_manny_emenike_Person_id_ref, brian_takita_Person_id_ref)=>
+			[dr_manny_emenike_Person_id_ref, brian_takita_Person_id_ref]))
