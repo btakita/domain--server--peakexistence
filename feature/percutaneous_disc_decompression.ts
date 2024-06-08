3e10c51ca96d02b41e8b0ff60d1b6064766c0c98
Brian Takita <brian.takita@gmail.com>
2024-06-08 14:25:06 -0400

    ./feature: percutaneous_spine_decompression→percutaneous_disc_decompression

---
+++ feature/percutaneous_disc_decompression.ts
@@ -0,0 +1,20 @@
+import hero_image from '../public/image/article/percutaneous-disc-decompression.webp'
+import { type feature_T } from './_subtle.js'
+export const percutaneous_disc_decompression = <feature_T>{
+	title: `Percutaneous Disc Decompression`,
+	description: `Percutaneous Disc Decompression is a minimally invasive surgical technique. Designed to alleviate pain caused by compressed nerves in the spine. This innovative procedure has become increasingly popular. As a treatment option for patients suffering from back conditions such as herniated discs, spinal stenosis, & radiculopathy.`,
+	// language=md
+	description_md: `
+Percutaneous Disc Decompression is a minimally invasive surgical technique. Designed to alleviate pain caused by compressed nerves in the spine. This innovative procedure has become increasingly popular. As a treatment option for patients suffering from back conditions such as:
+
+- herniated discs
+- spinal stenosis
+- radiculopathy
+	`.trim(),
+	slug: 'percutaneous-disc-decompression',
+	tag_a1: [
+		'minimally-invasive-spine-surgery',
+		'percutaneous-disc-decompression',
+	],
+	hero_image,
+}
