ea261cb59f0e58c1dd60299dddcafbcdda0f0259
Brian Takita <brian.takita@gmail.com>
2024-06-10 01:18:58 -0400

    + ./contact:
    	+ phone_number
    	+ phone_number_href
    	+ phone_number_fmt
    	+ call_us

---
+++ contact/index.ts
@@ -0,0 +1,4 @@
+export const phone_number = '+18189066900'
+export const phone_number_href = `tel:${phone_number}`
+export const phone_number_fmt = '818-906-6900'
+export const call_us = `Call Us ${phone_number_fmt}`
